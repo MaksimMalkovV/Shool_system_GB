bc36a021ee6e8160a3323a4362319e576b93f90f
Maksim Malkov <113548800+MaksimMalkovV@users.noreply.github.com>
2023-01-29 23:32:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import view
+from colorama import Style
+import os
+
+os.system('cls')
+print(Style.BRIGHT + '-'*15 + "Здравствуйте!" + '-'*15 +
+      "\nЭто информационная система позволяющая работать с учениками школы\n")
+view.button_push()
